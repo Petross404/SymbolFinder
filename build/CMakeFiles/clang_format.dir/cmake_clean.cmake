3c25651daa0a401728e07bbb5b99d9d7f110f16e
Petross404 <petross404@gmail.com>
2022-04-10 18:43:47 +0300

    delete build dir

--- build/CMakeFiles/clang_format.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clang_format"
-  "clang_format_"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clang_format.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/src/Scanner/CMakeFiles/scanner_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/scanner_autogen"
-  "scanner_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/scanner_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
